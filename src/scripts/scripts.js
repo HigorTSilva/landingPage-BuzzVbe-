5f163cd247a6b7172155d28f81b0f428de1eda64
Higor Terr <higorterrdasilva@gmail.com>
2024-03-11 20:04:01 -0300

    Fim Landing

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,16 +1,13 @@
 const navbar = document.querySelector(".navbar");
-const navMobile = document.querySelector("#nav__mobile");
-const closeNavMobile = document.querySelector("#nav__mobile");
+const button = document.querySelector(".burguer");
+const mobileNavbar = document.querySelector(".navbar__mobile");
+
+button.addEventListener("click", function() {
+    mobileNavbar.classList.toggle("active");
+})
+
 
 window.addEventListener('scroll', function () {
     if (this.window.scrollY > 0) return navbar.classList.add('active');
     return navbar.classList.remove('active');
 });
-
-function openNav() {
-    navMobile.style.width = "100%";
-}
-
-function closeNav() {
-    closeNavMobile.style.width = "0";
-}
