229bc239c71f97365d030ab34561e9b366bd5513
Higor Terr <higorterrdasilva@gmail.com>
2024-03-23 12:23:41 -0300

    BuzzVibe

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,6 +1,7 @@
 const navbar = document.querySelector(".navbar");
 const navMobile = document.querySelector("#nav__mobile");
 const closeNavMobile = document.querySelector("#nav__mobile");
+const body = document.querySelector("body");
 
 window.addEventListener('scroll', function () {
     if (this.window.scrollY > 0) return navbar.classList.add('active');
@@ -9,8 +10,10 @@ window.addEventListener('scroll', function () {
 
 function openNav() {
     navMobile.style.width = "100%";
+    body.style.overflow = "hidden"
 }
 
 function closeNav() {
     closeNavMobile.style.width = "0";
+    body.style.overflow = "scroll"
 }
